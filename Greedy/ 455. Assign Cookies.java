1b89e50a389977fab2da9629a41e90d95867e95d
weiliango <wei-liang@live.cn>
2017-04-29 14:55:16 -0700

    455

---
+++ Greedy/ 455. Assign Cookies.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public int findContentChildren(int [] g, int[] s) {
+		if( g.length == 0 || s.length == 0 ) return 0;
+		Arrays.sort(g);
+		Arrays.sort(s);
+
+		int p = 0;
+		for ( int i = 0; i < s.length; i++ ) {
+			if ( s[i] >= g[p] ) {
+				p++;
+				if ( p >= g.length) {
+					re
+				}
+			}
+		}
+
+		return p;
+	}
+}
