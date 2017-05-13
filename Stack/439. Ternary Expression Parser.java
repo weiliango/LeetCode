9970ef28297e7c45317b94c6d6ca67273f1c01eb
weiliango <wei-liang@live.cn>
2017-05-12 20:53:49 -0700

    2017-05-12 20:53:49-07:00

--- Stack/439. Ternary Expression Parser.java
+++ Stack/439. Ternary Expression Parser.java
@@ -1,5 +1,17 @@
 public class Solution {
     public String parseTernary(String expression) {
-        
+ 		Stack <Character> stack = new Stack <Character> ();
+ 		for ( int i = 0; i < expression.length() ; i++ ) {
+ 			char curr = expression.charAt(i);
+ 			if ( curr != ':' ) {
+ 				curr.push(curr);
+ 			} else {
+ 				StringBuilder sb = new StringBuilder();
+ 				while ( (char tmp = curr.pop()) != '?' ) {
+ 					sb.insert(0, tmp);
+ 				}
+ 				char condition = sb.pop();
+ 			}
+ 		}
     }
 }
