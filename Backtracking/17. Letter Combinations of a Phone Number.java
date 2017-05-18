51d2179ab1dcd4e8bc81dc513a2682b5c19bbe1a
weiliango <wei-liang@live.cn>
2017-05-18 09:59:53 -0700

    2017-05-18 09:59:53-07:00

---
+++ Backtracking/17. Letter Combinations of a Phone Number.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+    	for ( int i = 0; i < digits.length(); i++ ) {
+    	 	
+    	} 
+    }
+
+    public void backtrack() {
+
+    }
+}
