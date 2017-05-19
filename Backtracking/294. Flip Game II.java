d975334ff73f15230aeb0793665961451091d8a1
weiliango <wei-liang@live.cn>
2017-05-19 11:32:06 -0700

    2017-05-19 11:32:06-07:00

--- Backtracking/294. Flip Game II.java
+++ Backtracking/294. Flip Game II.java
@@ -1,8 +1,19 @@
 public class Solution {
     public boolean canWin(String s) {
- 	
+		return helper(new StringBuilder(s));        
     }
-    public boolean backtrack() {
-    	
+    public boolean helper(StringBuilder sb) {
+    	boolean win;
+    	for ( int i = 0; i < sb.length() - 1 ; i++ ) {
+    		if ( sb.charAt(i) == '+' && sb.charAt(i+1) == '+' ) {
+    			sb.replace(i, i+2, "--" );
+    			win = !helper(sb);
+    			sb.replace(i, i+2, "++" );
+    			if ( win ) {
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
     }
 }
---
+++ Stack/456. 132 Pattern.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        
+    }
+}
