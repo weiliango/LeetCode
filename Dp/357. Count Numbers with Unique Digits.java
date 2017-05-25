f55d4e5117867dea2460990f9eb6cadf7db9b8eb
weiliango <wei-liang@live.cn>
2017-05-25 13:54:09 -0700

    2017-05-25 13:54:09-07:00

--- Dp/357. Count Numbers with Unique Digits.java
+++ Dp/357. Count Numbers with Unique Digits.java
@@ -1,5 +1,9 @@
 public class Solution {
     public int countNumbersWithUniqueDigits(int n) {
- 		       
+    	int res = 0;
+ 		for ( int i = 1; i <= n ; i++ ) {
+ 			res = res * 10 * (n-1) + 9 * res;       	
+        }       
+        return Math.pow(10,n) - res;
     }
 }
