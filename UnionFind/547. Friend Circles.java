e80606c7de8e2c7739dc60d43e6ea42c74e5278a
Weiliango <weiliang@weiliang.localdomain>
2017-07-06 22:22:48 -0700

    quick commit: 2017-07-06 21:33:41-07:00

--- UnionFind/547. Friend Circles.java
+++ UnionFind/547. Friend Circles.java
@@ -1,5 +1,31 @@
 public class Solution {
+    int[] roots;
+    
     public int findCircleNum(int[][] M) {
-        
+        if(M == null || M.length < 1 || M[0].length < 1) return 0;
+        roots = new int[M.length];
+        int count = 0;
+        for(int i = 0; i < M.length; roots[i] = i, i++);
+        for(int i = 0; i < M.length; i++) {
+            for(int j = i + 1; j < M[0].length; j++) {
+                if(M[i][j] == 1) {
+                    int root1 = findRoot(i);
+                    int root2 = findRoot(j);
+                    if( root1 != root2 ) {
+                        roots[root2] = root1;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < roots.length; i++) {
+            if(roots[i] == i) count++;
+        }
+        return count;
+    }
+    public int findRoot(int n) {
+        while(roots[n] != n) {
+            n = roots[n];
+        }
+        return n;
     }
 }
